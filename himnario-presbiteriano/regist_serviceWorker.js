c1a922a21a83e1b67275969c3e05807a1f951ff4
LeonardoCahuichLopez <leonardo.cahuich@icloud.com>
2024-10-21 16:06:41 -0600

    Error de service workers reparado

--- himnario-presbiteriano/regist_serviceWorker.js
+++ himnario-presbiteriano/regist_serviceWorker.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./regist_serviceWorker.js').then(function (registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 var cacheName = "himnario-presbiteriano_v0.0.1";
 var filesToCache = [
     './favicon.ico',
