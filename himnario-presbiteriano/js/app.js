f746a1a2abcbb894fd6ed1e10ec79a82dc52404b
LeonardoCahuichLopez <leonardo.cahuich@icloud.com>
2024-10-21 19:43:11 -0600

    Versión

--- himnario-presbiteriano/js/app.js
+++ himnario-presbiteriano/js/app.js
@@ -7,7 +7,7 @@ webSiteData = {
   nameMarquet: 'Himnario <b>Prebisteriano</b>',
   author: 'Leonardo Cahuich',
   dev: 'Leonardo Cahuich',
-  version: '0.0.1.4.2',
+  version: '0.0.1.4.3',
 }
 
 function serviceWorkerInit() {
